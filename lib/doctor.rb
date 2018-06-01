3436f588b6eb189f5591d9a3b5ef92e2c1072277
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-06-01 22:54:15 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,9 +17,7 @@ class Doctor
   end
 
   def appointments
-
-    Appointments.all.select {|appointment| appointment.doctor == self}
-
+    Appointment.all.select {|appointment| appointment.doctor == self}
   end
 
 
